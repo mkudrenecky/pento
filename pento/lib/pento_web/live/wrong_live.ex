f794029c2adc00a3c8da4a019abd9a6599aaa667
mkudrenecky <mkudrene@student.ubc.ca>
2024-08-01 10:01:32 -0600

    random answer and winnning message

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -2,14 +2,20 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess homie:")}
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess homie:",
+       time: time(),
+       answer: :rand.uniform(10) |> to_string()
+     )}
   end
 
   def render(assigns) do
     ~H"""
     <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
     <h2>
-      <%= @message %>
+      <%= @message %> It's <%= @time %>
     </h2>
     <br />
     <h2>
@@ -27,8 +33,24 @@ defmodule PentoWeb.WrongLive do
   end
 
   def handle_event("guess", %{"number" => guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again."
-    score = socket.assigns.score - 1
-    {:noreply, assign(socket, message: message, score: score)}
+    time = time()
+    answer = socket.assigns.answer
+
+    cond do
+      guess == answer ->
+        score = socket.assigns.score + 1
+        message = "Correct"
+        {:noreply, assign(socket, message: message, score: score, time: time)}
+
+      true ->
+        score = socket.assigns.score - 1
+        message = "Wrong guess!, guess again"
+        {:noreply, assign(socket, message: message, score: score, time: time)}
+    end
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
   end
 end
+
